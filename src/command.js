2526222abac6e907d21282b06dfc5cadbee6ba01
akilsagitarius <akil.dcc@gmail.com>
2021-09-30 17:06:20 +0800

    git push automatic

--- src/command.js
+++ src/command.js
@@ -17,21 +17,26 @@ async function running(option) {
     console.log(chalk.green('Starting AutoBot development server : ' + now))
     cron.schedule('*/20 * * * * *', async () => {
         now = new Date()
-        await gitBot(option.message)
+        await gitBot(option)
         console.log(chalk.blue('Active date : ' + now))
         console.log('Running a task every minute, with message ' + option.message);
     })
 }
 
-async function gitBot(message) {
+async function gitBot(option) {
     let gitStatus = await git.status('-s')
     //console.log(gitStatus.modified.length)
     if(gitStatus.modified.length != 0 && gitValue === 0)
     {
-//         console.log('\nYou can write ' + message + ' for message commit and now Please commit')
+        console.log('\nConnecting to your repository, Please wait')
         table(gitStatus)
         gitValue++
-        console.log(await git.add('./*').commit(message))
+        console.log(await git.add('./*').commit(option.message))
+        if(typeof(option.branch) === 'undefined') {
+            console.log(await git.push())
+        } else {
+            console.log(await git.push('origin', option.branch))
+        }
     }
 }
 
