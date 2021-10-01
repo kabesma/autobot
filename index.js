4f5e0e9ce25dc2f460c1dc50937ef4e99c23ff2c
akilsagitarius <akil.dcc@gmail.com>
2021-10-01 13:59:10 +0800

    publish on

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = require("./lib");
+#! /usr/bin/env node
+
+const { running  } = require('./src/command.js')
+const { program } = require('commander')
+const chalk = require('chalk')
+const figlet = require('figlet')
+
+/**
+ * This main function for show all command
+ */
+async function main() {
+    program
+        .command('run')
+        .description(
+          'Use run for running bot'
+        )
+        .option('-m, --message <string>', 'Add message with optional type')
+        .option('-b, --branch <string>', 'Push to branch option')
+        .action(running)
+
+    console.log('')
+    console.log(chalk.red(figlet.textSync("\nAutoBot Git", { font: 'ANSI Shadow', horizontalLayout: 'full' })))
+    console.log(`Welcome to the AutoBot! 🔥 🔥 🔥`)
+    program.parse(process.argv)
+}
+
+main()
