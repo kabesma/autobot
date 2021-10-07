5cdf3572c3b4d2434f6c9a9dea507df923b8c30d
akilsagitarius <akil.dcc@gmail.com>
2021-10-07 23:00:00 +0800

    Bot

--- src/bot.js
+++ src/bot.js
@@ -1,22 +1,27 @@
-#! /usr/bin/env node
-const {commit, table} = require('./result.js') 
+const { commit, table } = require('./result.js') 
 const simpleGit = require('simple-git')
 const git = simpleGit()
-let gitValue = 0
+const path = require('path')
+const config = require('yaml-config')
+
+let relative = require.resolve('../autobots.yaml')
+let settings = config.readConfig(relative)
 
 async function bot(option) {
-    console.log('this is bot')
     let gitStatus = await git.status('-s')
-    if(gitStatus.modified.length != 0 && gitValue === 0)
+    if(gitStatus.modified.length != 0)
     {
         console.log('\nConnecting to your repository, Please wait')
         table(gitStatus)
-        gitValue++
-        commit(await git.add('./*').commit(option.message))
+        let message = option.message;
+        if(message === undefined){
+            message = settings.message[Math.floor(Math.random() * (settings.message.length - 1))]
+        }
+        console.log(message)
+        commit(await git.add('./*').commit(message))
         let branch = await git.branch()
         await git.pull('origin', branch.current)
         await git.push('origin', branch.current)
-        gitValue = 0
     }
 }
 
